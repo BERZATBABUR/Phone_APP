22a5e5d54c2c1f70e2404e4d2bba5b6871f06996
berzat <baburberzat@gmail.com>
2026-03-09 14:19:15 +0300

    Added new improvement

--- TelefonRehberApi/Models/Dtos/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace TelefonRehberApi.Models.Dtos;
-
-public class AuthResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public int UserId { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string? DisplayName { get; set; }
-}
